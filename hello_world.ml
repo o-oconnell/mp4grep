59aad0d8692e2b2ae6276dd1a67668bd751306df
o-oconnell <reamonoshea@gmail.com>
2022-01-06 17:50:23 -0700

    successfully created cross-compatibility!

--- hello_world.ml
+++ hello_world.ml
@@ -1,1 +1,2 @@
+external set_vosk_loglevel : int -> int = "callable_from_ocaml";;
 print_endline "hello, world"
--- vosk_test.c
+++ vosk_test.c
@@ -1,7 +1,11 @@
+#define CAML_NAME_SPACE
 #include "include/vosk_api.h"
+#include <caml/mlvalues.h>
+#include <caml/fail.h>
 #include <stdio.h>
 
-int main() {
+CAMLprim value callable_from_ocaml(value int_v) {
+    vosk_set_log_level(Int_val(int_v));
     FILE *wavin;
     char buf[3200];
     int nread, final;
@@ -25,6 +29,35 @@ int main() {
     vosk_recognizer_free(recognizer);
     vosk_model_free(model);
     fclose(wavin);
-    return 0;
+
+    printf("just set the vosk log level!\n");
+    return Val_int(12);
 }
+
+/* int main() { */
+/*     FILE *wavin; */
+/*     char buf[3200]; */
+/*     int nread, final; */
+
+/*     VoskModel *model = vosk_model_new("/home/ooc/mp4grep/model"); */
+/*     VoskRecognizer *recognizer = vosk_recognizer_new(model, 16000.0); */
+
+/*     wavin = fopen("harvard.wav", "rb"); */
+/*     fseek(wavin, 44, SEEK_SET); */
+/*     while (!feof(wavin)) { */
+/*          nread = fread(buf, 1, sizeof(buf), wavin); */
+/*          final = vosk_recognizer_accept_waveform(recognizer, buf, nread); */
+/*          if (final) { */
+/*              printf("%s\n", vosk_recognizer_result(recognizer)); */
+/*          } else { */
+/*              printf("%s\n", vosk_recognizer_partial_result(recognizer)); */
+/*          } */
+/*     } */
+/*     printf("%s\n", vosk_recognizer_final_result(recognizer)); */
+
+/*     vosk_recognizer_free(recognizer); */
+/*     vosk_model_free(model); */
+/*     fclose(wavin); */
+/*     return 0; */
+/* } */
 
